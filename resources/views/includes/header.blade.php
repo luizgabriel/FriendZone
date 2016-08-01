4428615e556642baefface6314369c61ec111555
Eric <eric.lins@outlook.com>
2016-08-01 09:14:28 -0300

    Fixed profile link

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -137,7 +137,7 @@
                         <!-- Menu Footer-->
                         <li class="user-footer">
                             <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Perfil</a>
+                                <a href="/profile" class="btn btn-default btn-flat">Perfil</a>
                             </div>
                             <div class="pull-right">
                                 <a href="{{ url('auth/logout') }}" class="btn btn-default btn-flat">Sair</a>

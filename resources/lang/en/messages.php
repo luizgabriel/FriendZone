0565dde342d652338a1176664492845a85c1a79b
EricHolanda <eric.lins@outlook.com>
2016-09-12 09:47:30 -0300

    Update messages.php
    Added en localizations.

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,4 +1,21 @@
 <?php
 return [
     '503' => 'Be right back',
+    
+    
+    'login' => [
+        'credentials' => 'Enter your credentials',
+        'submit' =>  'Sign in',
+        'forgot-pwd' => 'Forgot my password',
+        'register' => 'I want to register',
+        'remember-me' => 'Remember me',
+    ],
+    
+    'register'  => [
+        'be-part' => 'Be part of this Social Network!',
+        'submit' => 'Register',
+        'already_rgs' => 'You have an account yet? Go back.',
+        
+    ],
+    
 ];

3398c52414a7c48103253fbde78cd087e9a3a9c7
EricHolanda <eric.lins@outlook.com>
2016-09-12 09:18:42 -0300

    Update messages.php
    Added localizations on login.

--- resources/lang/pt-br/messages.php
+++ resources/lang/pt-br/messages.php
@@ -2,4 +2,13 @@
 
 return [
     '503' => 'Volto logo',
+    
+    
+    'login' => [
+      'submit' =>  'Entrar',
+      'forgot-pwd' => 'Esqueci minha senha',
+      'register' => 'Quero me registrar',
+      'remember-me' => 'Lembrar-se de mim',
+    ],
+    
 ];

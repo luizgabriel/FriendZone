ea1601c87bb65f33d809ff701c23b47a6e328e07
EricHolanda <eric.lins@outlook.com>
2016-09-14 08:08:23 -0300

    Update edit.blade.php
    Added localizations.

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -2,14 +2,14 @@
     <input type="hidden" name="_method" value="PUT">
     <input type="hidden" name="_token" value="{{ csrf_token() }}">
     <div class="form-group">
-        <label for="name" class="col-sm-1 control-label">Nome</label>
+        <label for="name" class="col-sm-1 control-label">@lang('messages.users.name')</label>
         <div class="col-md-11">
             <input type="text" name="name" class="form-control" value="{{ $user->name }}" placeholder="Nome">
         </div>
     </div>
     <div class="form-group">
         <div class="col-md-12">
-            <button type="submit" class="btn btn-danger">Atualizar Dados</button>
+            <button type="submit" class="btn btn-danger">@lang('messages.users.updt-rgster')</button>
         </div>
     </div>
 </form>

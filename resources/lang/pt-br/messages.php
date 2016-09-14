4a07717d4c26a8e99ac9d9dd3caa12720f13cce4
EricHolanda <eric.lins@outlook.com>
2016-09-14 08:20:17 -0300

    Update messages.php
    Added more localization messages.

--- resources/lang/pt-br/messages.php
+++ resources/lang/pt-br/messages.php
@@ -31,6 +31,43 @@ return [
         'accept' => 'Aceitar',
         'refuse' => 'Recusar',
         'friends-of' => 'Amigos de :name',
+        'friends' => 'Amigos',
+        'send' => 'Enviar solicitação de amizade',
+        'sent' => 'Solicitação de amizade enviada!',
+        'see-all' => 'Ver todos os amigos',
+        'posts' => 'Postagens',
+        'doesnt-have-posts' => 'não possui nenhuma postagem...',
     ],
+    
+    'header' => [
+        'followers' => 'Seguidores',
+        'friends' => 'Amigos',
+        'profile' => 'Perfil',
+        'logout' => 'Sair',
+    ],
+    
+    'sidebar' => [
+        'postshall' => 'Hall de Postagens',
+    ],
+    
+    'posts' => [
+        'whatdoya' => 'O que você está pensando?',
+        'post' => 'Postar',
+        'morecomments' => 'Ver mais comentários',
+        'post-of' => 'Postagem de',
+    ],
+    
+    'users' => [
+        'name' => 'Nome',
+        'updt-rgster' => 'Atualizar dados',
+        'edit' => 'Editar Perfil',
+        'updt-yer' => 'Atualize as informações do seu perfil',
+        
+    ],
+
+    'general' => [
+        'version' => 'Versão',
+        'about' => 'Este projeto não possui fins lucrativos.',
+    ],    
     
 ];

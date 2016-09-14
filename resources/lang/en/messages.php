13f27f5e823099b4b3c25ed6656daf606d20df2e
EricHolanda <eric.lins@outlook.com>
2016-09-14 08:27:04 -0300

    Update messages.php
    Added more en message localizations.

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -18,4 +18,54 @@ return [
         
     ],
     
+    'comment' => [
+        'submit' => 'Type your comment and press ENTER',    
+        
+    ],
+    
+    'friends' => [
+        'dont-have' => 'You don't have friend requests',
+        'requests' => 'Freind requests',
+        'sent' => 'You sent a friend request',
+        'accept' => 'Accept',
+        'refuse' => 'Refuse',
+        'friends-of' => ':name's friends,
+        'friends' => 'Friends',
+        'send' => 'Send a friend request',
+        'sent' => 'Friend request sent!',
+        'see-all' => 'See all friends',
+        'posts' => 'Posts',
+        'doesnt-have-posts' => 'doesn't have any post...'',
+    ],
+    
+    'header' => [
+        'followers' => 'Followers',
+        'friends' => 'Friends',
+        'profile' => 'Profile',
+        'logout' => 'Log out',
+    ],
+    
+    'sidebar' => [
+        'postshall' => 'Posts Hall',
+    ],
+    
+    'posts' => [
+        'whatdoya' => 'What do you have in mind?',
+        'post' => 'Post',
+        'morecomments' => 'See more comments',
+        'post-of' => 'Post of',
+    ],
+    
+    'users' => [
+        'name' => 'Name',
+        'updt-rgster' => 'Update register',
+        'edit' => 'Edit profile',
+        'updt-yer' => 'Update your profile information',
+        
+    ],
+    'general' => [
+        'version' => 'Version',
+        'about' => 'This is a non-profit project.',
+    ],    
+    
 ];

41280bff625ecc23996d924b2ed616dc106f8562
Luiz Gabriel <luizgabriel.info@gmail.com>
2016-09-21 08:47:53 -0300

    Update FriendRequestController.php

--- app/Http/Controllers/FriendRequestController.php
+++ app/Http/Controllers/FriendRequestController.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Http\Request;
 use FriendZone\Http\Requests;
 use FriendZone\FriendRequest;
+use FriendZone\User;
 
 class FriendRequestController extends Controller
 {
@@ -18,8 +19,9 @@ class FriendRequestController extends Controller
 
     public function answer(Request $request)
     {
+        $sender = User::findOrFail($request->get('sender_id'))
         if ($request->get('action') == 'accept')
-            $request->user()->addFriend($request->get('sender_id'));
+            $request->user()->addFriend($sender);
 
         return $this->destroy($request);
     }

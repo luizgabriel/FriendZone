64119baa6293c0e48fcfd51b803880deab0df629
Luiz Gabriel <luizgabriel.info@gmail.com>
2016-09-21 08:31:27 -0300

    Update show.blade.php

--- resources/views/friendrequests/show.blade.php
+++ resources/views/friendrequests/show.blade.php
@@ -1,15 +1,15 @@
 <a href="#">
     <div class="pull-left">
-        <img src="{{ url('img/user3-128x128.jpg') }}" class="img-circle" alt="User Image">
+        <img src="{{ $requester->photo_url }}" class="img-circle" alt="User Image">
     </div>
     <h4>
-        {{ $friendrequest->sender->name }}
-        <small><i class="fa fa-clock-o"></i>{{ $friendrequest->created_at->diffForHumans() }}</small>
+        {{ $requester->name }}
+        <small><i class="fa fa-clock-o"></i>{{ $requester->pivot->created_at->diffForHumans() }}</small>
     </h4>
     <p>@lang('messages.friends.sent')</p>
 
-    <form method="post" class="pull-right" action="{{ route('friendrequests.answer', $friendrequest) }}">
-        <input type="hidden" name="sender_id" value="{{ $friendrequest->sender->id }}">
+    <form method="post" class="pull-right" action="{{ route('friendrequests.answer') }}">
+        <input type="hidden" name="sender_id" value="{{ $requester->id }}">
 
         <div class="btn-group">
             <button type="submit" class="btn btn-xs btn-success" name="action" value="accept">
